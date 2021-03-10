98500913dd3478d3c05527bcf0ee7b1f3f977cec
bjornorn <bjorn.ornevik@hotmail.com>
2021-03-10 15:14:31 +0100

    start

--- JøranQuery/model.js
+++ JøranQuery/model.js
@@ -12,77 +12,77 @@ For hver side:
 
 // const obj2 = obj;
 const model = {
- 
-    infoBlocks: {
-        info1: 'header',    
-        info2: 'createNewQuery',
-        info3: 'watchActiveQueries',
-        info4: 'showEndedQueries',
-    },
-    
-    queryBlock: { 
-        addQueryText: 'Skriv navn på spørreundersøkelsen her:',
-        questionField: 'question',        
-        queryId: 'queryIdNumber',        
-    },
-    
-    queryQuestion1: {
-        questionField: 'question',    
-        answerField1: 'answer1',    
-        answerField2: 'answer2',    
-        answerField3: 'answer3', 
-    },
-    queryQuestion2: {
-        questionField: 'question',    
-        answerField1: 'answer1',    
-        answerField2: 'answer2',    
-        answerField3: 'answer3', 
-    },
+  //admin Navigation blocks
+  infoBlocks: {
+    info1: 'header',
+    info2: 'createNewQuery',
+    info3: 'watchActiveQueries',
+    info4: 'showEndedQueries',
+  },
+  // Questionere block
+  queryBlock: {
+    addQueryText: 'Skriv navn på spørreundersøkelsen her:',
+    questionField: 'question',
+    queryId: 'queryIdNumber',
+  },
+  //question blocks
+  queryQuestion1: {
+    questionField: 'question',
+    answerField1: 'answer1',
+    answerField2: 'answer2',
+    answerField3: 'answer3',
+  },
+  queryQuestion2: {
+    questionField: 'question',
+    answerField1: 'answer1',
+    answerField2: 'answer2',
+    answerField3: 'answer3',
+  },
 
-    queryQuestion3: {
-        questionField: 'question',    
-        answerField1: 'answer1',    
-        answerField2: 'answer2',    
-        answerField3: 'answer3', 
-    },
+  queryQuestion3: {
+    questionField: 'question',
+    answerField1: 'answer1',
+    answerField2: 'answer2',
+    answerField3: 'answer3',
+  },
 
-    queryQuestion4: {
-        questionField: 'question',    
-        answerField1: 'answer1',    
-        answerField2: 'answer2',    
-        answerField3: 'answer3', 
-    },
+  queryQuestion4: {
+    questionField: 'question',
+    answerField1: 'answer1',
+    answerField2: 'answer2',
+    answerField3: 'answer3',
+  },
 
-    addQuestion: {
-        add: false, 
-        addQuestionText: 'Skriv inn spørsmålet her',
-        questionInputField: 'Spørsmålstekst',
-    },
+  addQuestion: {
+    add: false,
+    addQuestionText: 'Skriv inn spørsmålet her',
+    questionInputField: 'Spørsmålstekst',
+  },
+  //Define ID
+  defineAnonymous: {
+    status: false,
+  },
+  // Define time
+  setDeadline: {
+    status: false,
+    date: 'dato',
+  },
+  // People
+  addParticipants: {
+    status: false,
+    participantName: 'navn',
+    participantEMail: 'epost',
+    participantGroupField1: 'Faste ansatte',
+    participantGroupField2: 'Deltid ansatte',
+    participantGroupField3: 'Styret',
+    participantGroupField4: 'Ringevikarer',
+    participantGroupField5: 'Jørans mamma',
+  },
+  // push the query
+  publishQuery: {
+    status: false,
+    publishButton: '',
+  },
 
-    defineAnonymous: {
-        status: false,        
-    },
-
-    setDeadline: {
-        status: false,
-        date: 'dato',               
-    },
-
-    addParticipants: {
-        status: false,
-        participantName: 'navn',               
-        participantEMail: 'epost', 
-        participantGroupField1: 'Faste ansatte',              
-        participantGroupField2: 'Deltid ansatte',              
-        participantGroupField3: 'Styret',              
-        participantGroupField4: 'Ringevikarer',              
-        participantGroupField5: 'Jørans mamma',              
-    },
-
-    publishQuery: {
-        status: false,
-        publishButton: '',
-    }
-    
+  //Query Result
 };
-
