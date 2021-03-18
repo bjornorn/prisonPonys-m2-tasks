c4d3973fc31c0c5811b226d46a5972d8894bc4ed
devapath89 <72752461+devapath89@users.noreply.github.com>
2021-03-18 16:05:47 +0100

    we

--- PonyTimeDerby/Dummy_model.js
+++ PonyTimeDerby/Dummy_model.js
@@ -4,4 +4,31 @@ var registerListMarch = {
     03: [125, '03.03.2021', '08:00', '11:30', '12:00', '16:00', 'PonyTimeDerby'],
     04: [126, '04.03.2021', '08:00', '11:30', '12:00', '16:00', 'PonyTimeDerby'],
     05: [127, '05.03.2021', '08:00', '11:30', '12:00', '16:00', 'PonyTimeDerby'],
+};
+
+var registeredToday = {
+    01: ['Antall timer', 'Prosjekt'],
+    02: ['1', 'PonyTimeDerby'],
+    03: ['0.5', 'Bjørns Bryggeblogg'],
+    04: ['1', 'Get Academy - Declaration of Freedom'],
+    05: ['2', 'PonyTimeDerby'],
+  
 }; 
+
+currentDate = new Date;
+
+dateIsNow = currentDate.toLocaleDateString();
+currentHour = currentDate.getHours();
+currentMinute = currentDate.getMinutes();
+timeIsNow = currentHour + ':' + currentMinute;
+
+// new prosject object
+
+let newProjectObject = {
+    projectId: 123,
+    projectName: 'Pony Derby Time',
+    todayDate: '17.03.2021',
+    deadlineDate: '08:00',
+    contributors: ['Dina', 'Andreas', 'Bjørn'],
+    
+};
--- PonyTimeDerby/PTDview.js
+++ PonyTimeDerby/PTDview.js
@@ -1,34 +1,58 @@
 let html = '';
 
-function updateView() {
-    html = '';
-    // html += `<div class='header'>${navButtons[0]}</div>`;
-    html += `<div class='sidebar'>`;
-    for (let i = 0; i < navButtons.length; i++) {
-        // html += `<div class='navButtons'>${i}</div>`;
-        html += `<button class='navButtons' type="button" onclick="${navView[i]}()">${navButtons[i]}</button>`;
-    }
-    html += `</div>`;    
-    document.getElementById("app").innerHTML = html;
+//***MAIN VIEW***
+    function updateView() {
+        html = '';
+        // html += `<div class='header'>${navButtons[0]}</div>`;
+        html += `<div class='sidebar'>`;
+        for (let i = 0; i < navButtons.length; i++) {
+            // html += `<div class='navButtons'>${i}</div>`;
+            html += `<button class='navButtons' type="button" onclick="${navView[i]}()">${navButtons[i]}</button>`;
+        }
+        html += `</div>`;    
+        document.getElementById("app").innerHTML = html;
 }
 
-function hoursRegisterSheet() {
-    html += `<div class="hoursRegisterSheet">`;
-    html += `<div><h2>Ny Registrering</h2></div>`;
-    for (let i = 0; i < hoursSheetForm.length; i++) {
-        html += `<div class="hoursRegisterSheetContent">${hoursSheetForm[i]}:</div>
-        <input type="text">        
-        </br>`;        
-    };
-    html += `<button class="hoursRegisterSheetContent">Registrer</button>`;
-    html += `</div>`;
-    document.getElementById("app").innerHTML = html;    
-}
+// ***TIME REGISTRERING VIEW***
+    // TEGNE OPP NY REGISTRERING FELT
+    function hoursRegisterSheet() {
+        html += `<div class="hoursRegisterSheet">`;
+        html += `<div><h2>Ny Registrering</h2></div>`;
+        for (let i = 0; i < hoursSheetForm.length; i++) {
+            html += `<div class="hoursRegisterSheetContent">${hoursSheetForm[i]}:<input type="text"></div>
+           
+            `;             
+        };
+        html += `<button class="hoursRegisterSheetContent reg" type="button">Registrer</button>`;
+        html += `</div>`;
 
+    // TEGNE OPP "TIMER I DAG FELT"
+        html += `<div class="todaysHoursClass"><h3>Registrerte timer i dag:</h3>`;
+        html += `<table><tr>`;
+        for (let i = 0; i < (Object.values(registeredToday).length); i++) {
+        html += `<td class="style1">${Object.values(registeredToday)[i][1]}</td> 
+                <td class="style2">${Object.values(registeredToday)[i][0]}</td>`
+        };
+        html += `<td class="style1">&nbsp</td><td class="style2">&nbsp</td>`;           
+        html += `<td class="style1">Sum Timer</td><td class="style2">4,5</td>`;           
+        html += `</tr></table></div>`;     
+            
+    // TEGNE OPP "KALENDER FELT"
+        html += `<div class="infoFieldClass">`;
+        html += `<div><h3>Timeregistering:</h3></div>`;
+        html += `<div>I dag er det ${dateIsNow}</div><br>`;
+        html += `<div>Klokken er: ${timeIsNow}</div>`;
+        html += `</div>`;
+        document.getElementById("app").innerHTML = html;    
+}
+// ***PROSJEKT REGISTERING VIEW***
+
+
+// ***TIME OVERSIKT VIEW***
 function hoursOverviewPage() {
     html += `<div class="hoursOverviewClass"><h2>Registrerte timer</h2></div>`;
     html += `<div>${registerListMarch}</div>`;
-    
-
     document.getElementById("app").innerHTML = html;    
 }
+
+// ***PROSJEKT OVERSIKT VIEW***
