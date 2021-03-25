c3b10359e261daeb9f34f6fb17f33cd2ed565140
bjornorn <bjorn.ornevik@hotmail.com>
2021-03-25 16:05:02 +0100

    fixin functions

--- PonyTimeDerby/PTDcontroller.js
+++ PonyTimeDerby/PTDcontroller.js
@@ -5,63 +5,64 @@
 // if(currentpage == "noe annet"){noeAnnetView()} osv
 
 function hoursRegisterView() {
-    updateView();
-    hoursRegisterSheetView();
+  updateView();
+  hoursRegisterSheetView();
 }
 
-
 function projectRegisterView() {
-    updateView();
-    projectRegisterPageView();
-    
-} 
+  updateView();
+  projectRegisterPageView();
+}
 
 function hoursOverviewView() {
-    updateView();
-    hoursOverviewPageView();
-    
-} 
+  updateView();
+  hoursOverviewPageView();
+}
 
 function projectOverviewView() {
-    updateView();
-    projectOverviewPageView();
+  updateView();
+  projectOverviewPageView();
 }
 
 // REGISTER TIME FUNCTIONS
 function registerCurrentHours() {
-
-    rightfulObj = model.registerData.hourActualValue[5];
-console.log(rightfulObj);
-for (let i = 0; i < Object.keys(theAProjects).length; i++)
-
-if (rightfulObj == Object.values(theAProjects)[i].projectName) {
-    theAProjects[Object.values(theAProjects)[i]].dateCreated = 'yes!';
-}
-
+  rightfulObj = model.registerData.hourActualValue[5];
+  for (let i = 0; i < Object.keys(theAProjects).length; i++) {
+    if (rightfulObj == Object.values(theAProjects)[i].projectName) {
+      rightfulObj = Object.values(theAProjects)[i];
+      keyCount = Object.keys(rightfulObj).length - 9 + 1;
+      datestamp = 'datestamp' + keyCount.toString();
+      rightfulObj[datestamp] = model.registerData.hourActualValue;
+      console.log(rightfulObj);
+      // console.log(datestamp);
+    }
+  }
 }
 
 // REGISTER PROJECT FUNCTIONS
 
-
 function registerActiveProject() {
-    recordCount++;
-    navn = 'projectNo' + recordCount.toString();
+  recordCount++;
+  navn = 'projectNo' + recordCount.toString();
 
-    theAProjects[navn] = new Object();
-    theAProjects[navn].projectId = model.registerData.actualValue[0];
-    theAProjects[navn].projectName = model.registerData.actualValue[1];
-    theAProjects[navn].budgetHours = model.registerData.actualValue[2];
-    theAProjects[navn].setDeadline = model.registerData.actualValue[3];
-    theAProjects[navn].dateCreated = '';
-    theAProjects[navn].dateFinished = '';
-    theAProjects[navn].sumTimeSpent = 0;
-    theAProjects[navn].sumTimeSpentToday = 0;
-    theAProjects[navn].datestampInfo = ['dateStamp', 'startTime', 'stoptime', 'pauseTrueFalse', 'pauseStart', 'pauseStop', 'sumHours', 'sumMinutes', 'totalTime'];
-  
+  theAProjects[navn] = new Object();
+  theAProjects[navn].projectId = model.registerData.actualValue[0];
+  theAProjects[navn].projectName = model.registerData.actualValue[1];
+  theAProjects[navn].budgetHours = model.registerData.actualValue[2];
+  theAProjects[navn].setDeadline = model.registerData.actualValue[3];
+  theAProjects[navn].dateCreated = '';
+  theAProjects[navn].dateFinished = '';
+  theAProjects[navn].sumTimeSpent = 0;
+  theAProjects[navn].sumTimeSpentToday = 0;
+  theAProjects[navn].datestampInfo = [
+    'dateStamp',
+    'startTime',
+    'stoptime',
+    'pauseTrueFalse',
+    'pauseStart',
+    'pauseStop',
+    'sumHours',
+    'sumMinutes',
+    'totalTime',
+  ];
 }
-
-
-
- 
-
-  
--- PonyTimeDerby/PTDview.js
+++ PonyTimeDerby/PTDview.js
@@ -20,21 +20,27 @@ function hoursRegisterSheetView() {
   html += `<div><h2>Ny Registrering</h2></div>`;
   html += `<table class="hoursRegisterSheetContent">`;
   for (let i = 0; i < model.hoursSheetForm.description.length; i++) {
-    html += `<tr><td>${Object.values(model.hoursSheetForm.description)[i]}:</td></tr>`;
-    }
-    html += `</table><table class="hoursRegisterSheetContent2">`;
-    for (let i = 0; i < (model.hoursSheetForm.description.length -1) ; i++) {
-     html += `<tr><td><input type="${model.registerData.dtProjectActiveRegister[i]}" 
+    html += `<tr><td>${
+      Object.values(model.hoursSheetForm.description)[i]
+    }:</td></tr>`;
+  }
+  html += `</table><table class="hoursRegisterSheetContent2">`;
+  for (let i = 0; i < model.hoursSheetForm.description.length - 1; i++) {
+    html += `<tr><td><input type="${model.registerData.dtProjectActiveRegister[i]}" 
               oninput="model.registerData.hourActualValue[${i}] = this.value"/></td></tr>`;
-        }  
-    //Dropdown meny
-  html += `<tr><td><select style="width: 200px" oninput="model.registerData.hourActualValue[${5}] = this.value">`;
-          for (let i = 0; i < Object.keys(theAProjects).length; i++) {     
-          html += `<option ${i} value="${Object.values(theAProjects)[i].projectName}">${Object.values(theAProjects)[i].projectName}</option>`;
-    }
-          `</select></tr></td>`;
+  }
+  //Dropdown meny
+  html += `<tr><td><select id="psel" style="width: 200px" oninput="model.registerData.hourActualValue[${5}] = this.value">`;
+  for (let i = 0; i < Object.keys(theAProjects).length; i++) {
+    html += `<option ${i}>${
+      Object.values(theAProjects)[i].projectName
+    }</option>`;
+  }
+  // value="${Object.values(theAProjects)[i].projectName}" LA STÅ!
+
+  html += `<option selected="selected"></option></select></tr></td>`;
   html += `</table>`;
-  html += `<button class="button">Registrer</button>`;
+  html += `<button class="button" onclick="registerCurrentHours()">Registrer</button>`;
   html += `</div>`;
 
   // TEGNE OPP "TIMER I DAG FELT"
@@ -43,12 +49,15 @@ function hoursRegisterSheetView() {
   `;
   let todaysHours = 0;
   for (let i = 0; i < Object.keys(theAProjects).length; i++) {
-    html += `<tr><td class="style1">${Object.values(theAProjects)[i].projectName}</td> 
-             <td class="style2">${Object.values(theAProjects)[i].sumTimeSpentToday}</td></tr>`;
-             todaysHours += parseFloat(Object.values(theAProjects)[i].sumTimeSpentToday);
-           
-            }
-  
+    html += `<tr><td class="style1">${
+      Object.values(theAProjects)[i].projectName
+    }</td> 
+             <td class="style2">${
+               Object.values(theAProjects)[i].sumTimeSpentToday
+             }</td></tr>`;
+    todaysHours += parseFloat(Object.values(theAProjects)[i].sumTimeSpentToday);
+  }
+
   html += `<tr><td class="style1">&nbsp</td><td class="style2">&nbsp</td></tr>`;
   html += `<tr><td class="style1">Sum Timer</td><td class="style2">${todaysHours}</td>`;
   html += `</tr></table></div>`;
@@ -64,7 +73,7 @@ function hoursRegisterSheetView() {
 // ***PROSJEKT REGISTERING VIEW***
 function projectRegisterPageView() {
   html += `<div class="projectRegisterSheet"><table class="projectRegisterSheetContent">`;
-  for (let i = 0; i < (Object.keys(model.projectInfoRegister).length - 4); i++) {
+  for (let i = 0; i < Object.keys(model.projectInfoRegister).length - 4; i++) {
     html += `<tr class="projectRegisterSheetContent"><td>${
       Object.values(model.projectInfoRegister)[i]
     }</td>
@@ -79,29 +88,26 @@ function projectRegisterPageView() {
   document.getElementById('app').innerHTML = html;
 }
 
-
-
 // ***TIME OVERSIKT VIEW***
 function hoursOverviewPageView() {
-
   html += `<table class="styleMonth"><tr>`;
-html +=`<td><a href="#"> Forrige måned</a></td> `
-html += `<td><div><b>Janumary</b></div></td>`; /* input month for change */
-html +=`<td><a href="#">Neste måned</a></td>`
-html += `</tr></table>`;
+  html += `<td><a href="#"> Forrige måned</a></td> `;
+  html += `<td><div><b>Janumary</b></div></td>`; /* input month for change */
+  html += `<td><a href="#">Neste måned</a></td>`;
+  html += `</tr></table>`;
 
- 
-
-  html+= `<div class="TimeRegister">`;
+  html += `<div class="TimeRegister">`;
   html += `<table><tr>`;
-  
+
   for (let i = 0; i < Object.values(hoursJanuary.dInfo).length; i++) {
-    html += `<td class="styleTimereg">${Object.values(hoursJanuary.dInfo)[i]}</td>`;    
+    html += `<td class="styleTimereg">${
+      Object.values(hoursJanuary.dInfo)[i]
+    }</td>`;
   }
   html += `</tr></table>`;
-  html+= `<div class="TimeRegister2">`;
+  html += `<div class="TimeRegister2">`;
   html += `<table>`;
-  
+
   for (let i = 1; i < Object.keys(hoursJanuary).length; i++) {
     html += `<tr>
        <td class="styleTimereg">${Object.values(hoursJanuary)[i][0]}</td>
@@ -112,26 +118,21 @@ html += `</tr></table>`;
        <td class="styleTimereg">${Object.values(hoursJanuary)[i][5]}</td>       
             </tr>`;
   }
-  
+
   html += `</table> </div></div>`;
-  
-  
 
   document.getElementById('app').innerHTML = html;
-} 
-
-
-
-
-
-
-
+}
 
 // ***PROSJEKT OVERSIKT VIEW***
 function projectOverviewPageView() {
   html += `<div class="projectRegisterSheet"><table class="projectRegisterSheetContent"><tr>`;
 
-  for (let i = 0; i < Object.values(model.projectActiveRegister)[0].length; i++) {
+  for (
+    let i = 0;
+    i < Object.values(model.projectActiveRegister)[0].length;
+    i++
+  ) {
     html += `<td>${Object.values(model.projectActiveRegister)[0][i]}</td>`;
   }
   html += `</tr>`;
